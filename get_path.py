ab4d9ebb658fb85bbd51928bca53196dee6825af
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-07-11 10:43:47 +0800

    Create get_path.py

---
+++ get_path.py
@@ -0,0 +1,13 @@
+# 取得程式當前路徑
+os.getcwd()
+
+# 取得運行的module當前的路徑
+os.path.abspath(os.path.dirname(__file__))
+
+print('***获取上级目录***')
+print(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+print(os.path.abspath(os.path.dirname(os.getcwd())))
+print(os.path.abspath(os.path.join(os.getcwd(), "..")))
+
+print('***获取上上级目录***')
+print(os.path.abspath(os.path.join(os.getcwd(), "../..")))
