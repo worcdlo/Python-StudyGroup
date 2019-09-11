62e7899e0215f257e92417b3acf78942813653b5
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-09-11 11:42:56 +0800

    Update demo_torch_dataset.py

--- Torch/demo_torch_dataset.py
+++ Torch/demo_torch_dataset.py
@@ -1,26 +1,37 @@
 import torch
 import torch.utils.data as Data
 
-torch.manual_seed(1)    # 打亂數據的的隨機種子
+# 打亂數據的的隨機種子
+torch.manual_seed(1)
 
-BATCH_SIZE = 8      # 每個批次的數據個數
+# 每個批次的數據個數
+BATCH_SIZE = 8
 
-x = torch.linspace(1, 10, 10)       # x data (torch tensor)
-y = torch.linspace(21, 30, 10)       # y data (torch tensor)
+# 打亂數據的的隨機種子
+torch.manual_seed(1)
+
+# 每個批次的數據個數
+BATCH_SIZE = 8
+
+# generate x, y in type: torch.tensor
+x = torch.linspace(1, 10, 10)
+y = torch.linspace(21, 30, 10)
 
 # 將資料轉換成 torch 能吃的 Dataset
 torch_dataset = Data.TensorDataset(x, y)
 
 # 把 dataset 放入 DataLoader
 loader = Data.DataLoader(
-    dataset=torch_dataset,      # torch TensorDataset format
-    batch_size=BATCH_SIZE,      # mini batch size
-    shuffle=True                # 是否打亂數據，可以減少訓練時overfitting產生，也能根據loss觀察是否存在overfitting
+    dataset=torch_dataset,       # torch TensorDataset format
+    batch_size=BATCH_SIZE,       # mini batch size
+    shuffle=True,                # 是否打亂數據
+    #num_workers=2,              # Multithread讀取数据，不知道為何總是失敗
 )
 
-# 訓練數據3次
+# 訓練數據3輪
 for epoch in range(3):
-    for step, (batch_x, batch_y) in enumerate(loader):  # 每一步loader都會釋放一小批數據來學習
+    # 每一輪的每一步loader都會釋放一小批數據來學習
+    for step, (batch_x, batch_y) in enumerate(loader):
         print("Epoch: {epoch} | Step: {step} | batch x: {x} | batch y: {y}".format(
                 epoch=epoch,
                 step=step,
@@ -28,6 +39,7 @@ for epoch in range(3):
                 y=batch_y.numpy()
             )
         )
+
 
 """Result
 每一梯次都隨機挑出 BATCH_SIZE 筆數據為一組訓練
@@ -39,10 +51,10 @@ shuffle除了打亂數據減少訓練時的overfitting外
 則在接下來的梯次中
 可能會發現loss急速上升
 
-Epoch: 0 | Step: 0 | batch x: [ 9.  7.  2. 10.  8.  4.  6.  1.] | batch y: [29. 27. 22. 30. 28. 24. 26. 21.]
-Epoch: 0 | Step: 1 | batch x: [5. 3.] | batch y: [25. 23.]
-Epoch: 1 | Step: 0 | batch x: [10.  2.  9.  5.  6.  4.  8.  7.] | batch y: [30. 22. 29. 25. 26. 24. 28. 27.]
-Epoch: 1 | Step: 1 | batch x: [1. 3.] | batch y: [21. 23.]
-Epoch: 2 | Step: 0 | batch x: [7. 2. 8. 9. 6. 5. 3. 1.] | batch y: [27. 22. 28. 29. 26. 25. 23. 21.]
-Epoch: 2 | Step: 1 | batch x: [10.  4.] | batch y: [30. 24.]
+Epoch: 0 | Step: 0 | batch x: [ 5.  7. 10.  3.  4.  2.  1.  8.] | batch y: [25. 27. 30. 23. 24. 22. 21. 28.]
+Epoch: 0 | Step: 1 | batch x: [9. 6.] | batch y: [29. 26.]
+Epoch: 1 | Step: 0 | batch x: [ 4.  6.  7. 10.  8.  5.  3.  2.] | batch y: [24. 26. 27. 30. 28. 25. 23. 22.]
+Epoch: 1 | Step: 1 | batch x: [1. 9.] | batch y: [21. 29.]
+Epoch: 2 | Step: 0 | batch x: [ 4.  2.  5.  6. 10.  3.  9.  1.] | batch y: [24. 22. 25. 26. 30. 23. 29. 21.]
+Epoch: 2 | Step: 1 | batch x: [8. 7.] | batch y: [28. 27.]
 """
