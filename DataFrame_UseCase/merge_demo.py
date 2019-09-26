78b5a5a60f53cd1d4fef307e4e04018e53478504
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-09-26 21:42:25 +0800

    Update merge_demo.py

--- DataFrame_UseCase/merge_demo.py
+++ DataFrame_UseCase/merge_demo.py
@@ -1,5 +1,25 @@
 import pandas as pd
 
+"""
+參考連結:https://blog.csdn.net/zutsoft/article/details/51498026
+
+merge(left, right, how='inner', on=None, left_on=None, right_on=None,
+      left_index=False, right_index=False, sort=True,
+      suffixes=('_x', '_y'), copy=True, indicator=False)
+
+- left與right：兩個不同的DataFrame
+- how：指的是合併(連接)的方式有inner(內連接),left(左外連接),right(右外連接),outer(全外連接);默認為inner
+- on : 指的是用於連接的列索引名稱。必須存在右右兩個DataFrame對像中，如果沒有指定且其他參數也未指定則以兩個DataFrame的列名交集做為連接鍵
+- left_on：左則DataFrame中用作連接鍵的列名;這個參數中左右列名不相同，但代表的含義相同時非常有用。
+- right_on：右則DataFrame中用作 連接鍵的列名
+- left_index：使用左則DataFrame中的行索引做為連接鍵
+- right_index：使用右則DataFrame中的行索引做為連接鍵
+- sort：默認為True，將合併的數據進行排序。在大多數情況下設置為False可以提高性能
+- suffixes：字符串值組成的元組，用於指定當左右DataFrame存在相同列名時在列名後面附加的後綴名稱，默認為('_x','_y')
+- copy：默認為True,總是將數據複製到數據結構中；大多數情況下設置為False可以提高性能
+- indicator：在 0.17.0中還增加了一個顯示合併數據中來源情況；如只來自己於左邊(left_only)、兩者(both)
+"""
+
 time = 'time'
 col1 = 'col1'
 col2 = 'col2'
